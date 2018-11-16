6abeff4c5bea0a46fbef9460dd0fbbe768853fee
uday721 <b.udayshanker@outlook.com>
2018-11-15 16:13:29 -0800

    IntialCommit

---
+++ SuperResolution/pch.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+//#include <wrl.h>
+//#include <wrl/client.h>
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include "Common\d3dx12.h"
+#include <pix.h>
+#include <DirectXColors.h>
+#include <DirectXMath.h>
+#include <memory>
+#include <vector>
+//#include <agile.h>
+#include <concrt.h>
+
+//Winrt header files
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.Graphics.h>
+#include <winrt/Windows.Graphics.Display.h>
+#include <winrt/Windows.UI.Xaml.h>
+
+
+#if defined(_DEBUG)
+#include <dxgidebug.h>
+#endif
